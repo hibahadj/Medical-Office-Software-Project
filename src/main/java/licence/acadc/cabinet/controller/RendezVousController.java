7568cb83bf9e9a64eef91b71eafd993f925cf3cb
chahinez26 <chahinez.benaicha2003@gmail.com>
2024-05-22 06:35:21 +0100

    Liste de RDV Reglée

--- src/main/java/licence/acadc/cabinet/controller/RendezVousController.java
+++ src/main/java/licence/acadc/cabinet/controller/RendezVousController.java
@@ -1,49 +1,54 @@
-
 package licence.acadc.cabinet.controller;
 
-//import java.io.IOException;
-//import java.io.InputStream;
-
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
-import java.util.ArrayList;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import licence.acadc.cabinet.modele.entity.Fichier;
+import licence.acadc.cabinet.modele.facade.CabUserFacade;
+import org.primefaces.PrimeFaces;
+import org.primefaces.event.FileUploadEvent;
 import org.primefaces.event.RowEditEvent;
 import org.primefaces.event.SelectEvent;
-import java.util.Arrays;
-import java.util.List;
-
+import org.primefaces.model.file.UploadedFile;
 
 import licence.acadc.cabinet.modele.entity.RendezVous;
 import licence.acadc.cabinet.modele.facade.RendezVousFacade;
+import org.apache.commons.fileupload.util.Streams;
+import org.primefaces.model.file.UploadedFiles;
+import org.primefaces.shaded.commons.io.FilenameUtils;
+import org.primefaces.shaded.commons.io.IOUtils;
 
 @Named
 @ViewScoped
-
-
 public class RendezVousController implements Serializable {
 
+    
     @Inject
-    private RendezVousFacade RendezVousFacade;
+    private RendezVousFacade rendezVousFacade;
+    private Date sysDate = new Date();
+
     private RendezVous entity;
-    private List<RendezVous> rdv = null;
-        private List<String> etatOptions = Arrays.asList("plannifié", "terminé", "annulé");
+    private List<RendezVous> listAll;
 
-    
-
-    
     @PostConstruct
     public void init() {
-          rdv = RendezVousFacade.findAll();
-
+        listAll = rendezVousFacade.findAll();
     }
 
-    
+    public RendezVousController() {
+    }
 
     public void onRowSelect(SelectEvent event) {
         entity = (RendezVous) event.getObject();
@@ -51,32 +56,35 @@ public class RendezVousController implements Serializable {
         //editMode = true;
     }
 
-    public void createPMedicamentt() {
-        RendezVousFacade.create(entity);
+    public void onInitRendezVous() {
+        entity = new RendezVous();
     }
 
-    public void onRowEditMedicament(RowEditEvent event) {
-        RendezVousFacade.edit(entity);
-        
+    public void createRendezVous() {
+        rendezVousFacade.create(entity);
+        listAll = rendezVousFacade.findAll();
+        entity = new RendezVous();
+        PrimeFaces.current().executeScript("PF('rendezVousDlg').hide()");
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info :", "Le Rendez-vous a été enregistrée avec succès"));
     }
 
-    public void removeMedicament() throws Exception {
-        RendezVousFacade.remove(entity);
+    public void onRowEditRendezVous(RowEditEvent event) {
+        rendezVousFacade.edit(entity);
+
     }
 
-    /*
-    public void uploadPatient(FileUploadEvent event) {
-        try {
-            UploadedFile uploadedFile = (UploadedFile) event.getFile();
-            //InputStream inputStr = uploadedFile.getInputstream();
-            //file = IOUtils.toByteArray(inputStr);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info :", "le fichier " + uploadedFile.getFileName() + " est chargé."));
-        } catch (Exception e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erreur :", "Erreur Inconnue"));
-        }
+    public void removerdv() {
+        rendezVousFacade.remove(entity);
+        listAll.remove(entity);
     }
-     */
+
+  
     
+    public static String getUserPrincipalRequest() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        return request.getUserPrincipal().toString();
+    }
+
     public RendezVous getEntity() {
         return entity;
     }
@@ -86,25 +94,13 @@ public class RendezVousController implements Serializable {
     }
 
     public List<RendezVous> getListAll() {
-        return rdv;
+        return listAll;
     }
 
     public void setListAll(List<RendezVous> listAll) {
-        this.rdv = listAll;
+        this.listAll = listAll;
     }
-    
-     public List<String> getEtatOptions() {
-        return etatOptions;
-    }
-
-    public void setEtatOptions(List<String> etatOptions) {
-        this.etatOptions = etatOptions;
-    }
-    
-    
-
-   
-    
-   
-    
 }
+
+    
+    
